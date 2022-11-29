e57df38b72f359bba76bf7099097148924bcc032
MATHEUS GOMES <matheusgsaraujo@gmail.com>
2022-11-28 22:53:58 -0300

    atualizações de resultados

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function clicou(group){
             `
             <div class='voltar' onclick='iniciar()'> Menu</div>
             ${createcard('22/11', 'terça', creategame('argentina','1 - 2','saudi arabia')+creategame('mexico','0 - 0','poland'))}
-            ${createcard('26/11', 'sábado', creategame('poland','10:00','saudi arabia')+creategame('argentina','16:00','mexico'))}
+            ${createcard('26/11', 'sábado', creategame('poland','2 - 0','saudi arabia')+creategame('argentina','2 - 0','mexico'))}
             ${createcard('30/11', 'quarta', creategame('poland','13:00','argentina')+creategame('saudi arabia','16:00','mexico'))}
             `
         break;
@@ -66,7 +66,7 @@ function clicou(group){
             `
             <div class='voltar' onclick='iniciar()'> Menu</div>
             ${createcard('22/11', 'terça', creategame('denmark','0 - 0','tunisia')+creategame('france','4 - 1','australia'))}
-            ${createcard('26/11', 'sábado', creategame('tunisia','7:00','australia')+creategame('france','13:00','denmark'))}
+            ${createcard('26/11', 'sábado', creategame('tunisia','0 - 1','australia')+creategame('france','2 - 1','denmark'))}
             ${createcard('30/11', 'quarta', creategame('tunisia','7:00','france')+creategame('australia','10:00','denmark'))}
             `
         break;
@@ -76,7 +76,7 @@ function clicou(group){
             `
             <div class='voltar' onclick='iniciar()'> Menu</div>
             ${createcard('23/11', 'quarta', creategame('germany','1 - 2','japan')+creategame('spain','7 - 0','costa rica'))}
-            ${createcard('27/11', 'domingo', creategame('japan','7:00','costa rica')+creategame('spain','16:00','germany'))}
+            ${createcard('27/11', 'domingo', creategame('japan','0 - 1','costa rica')+creategame('spain','1 - 1','germany'))}
             ${createcard('01/12', 'quinta', creategame('japan','13:00','spain')+creategame('costa rica','16:00','germany'))}
             `
         break;
@@ -86,7 +86,7 @@ function clicou(group){
             `
             <div class='voltar' onclick='iniciar()'> Menu</div>
             ${createcard('23/11', 'quarta', creategame('morocco','0 - 0','croatia')+creategame('belgium','1 - 0','canada'))}
-            ${createcard('27/11', 'domingo', creategame('belgium','10:00','morocco')+creategame('croatia','13:00','canada'))}
+            ${createcard('27/11', 'domingo', creategame('belgium','0 - 2','morocco')+creategame('croatia','4 - 1','canada'))}
             ${createcard('01/12', 'quinta', creategame('croatia','7:00','belgium')+creategame('canada','10:00','morocco'))}
             `
         break;
@@ -96,7 +96,7 @@ function clicou(group){
             `
             <div class='voltar' onclick='iniciar()'> Menu</div>
             ${createcard('24/11', 'quinta', creategame('switzerland','1 - 0','cameroon')+creategame('brazil','2 - 0','serbia'))}
-            ${createcard('28/11', 'segunda', creategame('cameroon','7:00','serbia')+creategame('brazil','13:00','switzerland'))}
+            ${createcard('28/11', 'segunda', creategame('cameroon','3 - 3','serbia')+creategame('brazil','1 - 0','switzerland'))}
             ${createcard('02/12', 'sexta', creategame('serbia','7:00','switzerland')+creategame('cameroon','16:00','brazil'))}
             `
         break;
@@ -106,7 +106,7 @@ function clicou(group){
             `
             <div class='voltar' onclick='iniciar()'> Menu</div>
             ${createcard('24/11', 'sexta', creategame('uruguay','0 - 0','south korea')+creategame('portugal','3 - 2','ghana'))}
-            ${createcard('28/11', 'segunda', creategame('south korea','10:00','ghana')+creategame('portugal','16:00','uruguay'))}
+            ${createcard('28/11', 'segunda', creategame('south korea','2 - 3','ghana')+creategame('portugal','2 - 0','uruguay'))}
             ${createcard('02/12', 'sexta', creategame('south korea','7:00','portugal')+creategame('ghana','10:00','uruguay'))}
             `
         break;
@@ -117,6 +117,7 @@ function iniciar(){
     document.querySelector("#app").innerHTML = `
     <form>
         <main id="cards">
+        
         ${createcardgroup("A")}
         ${createcardgroup("B")}
         ${createcardgroup("C")}
